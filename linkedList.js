42d9712b545fa1967d33de99c21efec973fc8708
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-06-01 22:32:44 -0600

    Returns the first node in the list

--- linkedList.js
+++ linkedList.js
@@ -2,12 +2,19 @@ const LinkedList = function () {
   let head = null;
   let size = 0;
 
+  // Adds a new node containing value to the start of the list
   let preppend = (value) => {
     head = node(value, head);
     size++;
   };
 
-  return { preppend };
+  // Returns the total number of nodes in the list
+  let getSize = () => size;
+
+  // Returns the first node in the list
+  let getHead = () => head.value;
+
+  return { preppend, getSize, getHead };
 };
 
 const node = function (value, next = null) {
@@ -18,3 +25,6 @@ let instanceOfLinkedList = LinkedList();
 //console.log(instanceOfLinkedList);
 instanceOfLinkedList.preppend(1);
 instanceOfLinkedList.preppend(5);
+instanceOfLinkedList.preppend("Diego");
+console.log(instanceOfLinkedList.getSize());
+console.log(instanceOfLinkedList.getHead());
