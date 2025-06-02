ad9304a947cc1bd09cf98be42d02a663d4075d0f
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-06-02 16:25:37 -0600

    Import module functions

---
+++ main.js
@@ -0,0 +1,20 @@
+import { LinkedList } from "./linkedList.js";
+
+let instanceOfLinkedList = LinkedList();
+//console.log(instanceOfLinkedList);
+instanceOfLinkedList.append(4);
+instanceOfLinkedList.preppend(1);
+instanceOfLinkedList.append(5);
+instanceOfLinkedList.append("Diego");
+instanceOfLinkedList.append("Samayoa");
+//console.log("Size", instanceOfLinkedList.getSize());
+//console.log("Head", instanceOfLinkedList.getHead());
+//console.log("Tail", instanceOfLinkedList.getTail());
+//console.log("At Index", instanceOfLinkedList.atIndex(0));
+//console.log("xd");
+//instanceOfLinkedList.toString();
+//instanceOfLinkedList.pop();
+//console.log("Size", instanceOfLinkedList.getSize());
+//console.log(instanceOfLinkedList.contains(5));
+//console.log(instanceOfLinkedList.find("Samayoa"));
+console.log(instanceOfLinkedList.toString());
