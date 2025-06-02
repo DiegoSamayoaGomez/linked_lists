7f8fd3338e8d89bf54e9dd997ce3f4f454dabadd
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-06-02 16:25:22 -0600

    Convert linkedList into a module

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,4 @@
-const LinkedList = function () {
+export const LinkedList = function () {
   let head = null;
   let size = 0;
 
@@ -128,22 +128,3 @@ const LinkedList = function () {
 const node = function (value, next = null) {
   return { value, next };
 };
-
-let instanceOfLinkedList = LinkedList();
-//console.log(instanceOfLinkedList);
-instanceOfLinkedList.append(4);
-instanceOfLinkedList.preppend(1);
-instanceOfLinkedList.append(5);
-instanceOfLinkedList.append("Diego");
-instanceOfLinkedList.append("Samayoa");
-//console.log("Size", instanceOfLinkedList.getSize());
-//console.log("Head", instanceOfLinkedList.getHead());
-//console.log("Tail", instanceOfLinkedList.getTail());
-//console.log("At Index", instanceOfLinkedList.atIndex(0));
-//console.log("xd");
-//instanceOfLinkedList.toString();
-//instanceOfLinkedList.pop();
-//console.log("Size", instanceOfLinkedList.getSize());
-//console.log(instanceOfLinkedList.contains(5));
-//console.log(instanceOfLinkedList.find("Samayoa"));
-//console.log(instanceOfLinkedList.toString());
